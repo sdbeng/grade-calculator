d64507f39ca60f1c7030cac6f6ebedff2dcdf28b
Sergio Barrera <sdbeng@gmail.com>
2018-04-21 07:32:37 -0700

    Add conditional logic

--- grade-calc.js
+++ grade-calc.js
@@ -15,10 +15,19 @@ let gradeCalc = (score, totalScore) => {
     let percent = (score / totalScore) * 100 //i.e 75%
     if(percent >= 90){
         console.log(`You got an A (${percent}%)!`);
-    }else if(percent >= 80 && percent < 89){
+    }
+    else if(percent >= 80 && percent < 89){
         console.log(`You got a B (${percent}%)!`);
+    }
+    else if(percent >= 70 && percent < 79){
+        console.log(`You got a C (${percent}%)!`);
+    }
+    else if(percent >= 60 && percent < 69){
+        console.log(`You got a C (${percent}%)!`);
+    }else{
+        console.log(`You got an F (${percent}%)!`);
     }
 }
 
 //test func
-gradeCalc(17, 20)
+gradeCalc(1, 20)
